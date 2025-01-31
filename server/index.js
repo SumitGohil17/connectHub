3dd837c8d6194674724d624a7beedfd8cfc2aa40
SumitGohil17 <118886308+SumitGohil17@users.noreply.github.com>
2025-01-31 17:51:14 +0530

    Add ping timeout and interval settings to socket server configuration

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ const io = new Server(server, {
         methods: ["GET", "POST"],
         credentials: true 
     },
+    pingTimeout: 60000,
+    pingInterval: 25000,
     allowEIO3: true
 });
 const port = 5001 || process.env.PORT;
